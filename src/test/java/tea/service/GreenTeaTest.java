85c3d536783f7873bc6c7b22df4d4fb40756ce21
kmolodtsov <nectarius@outlook.com>
2016-04-17 00:39:44 +0400

    add new tests

---
+++ src/test/java/tea/service/GreenTeaTest.java
@@ -0,0 +1,94 @@
+package tea.service;
+
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.testng.Assert;
+
+import tea.service.impl.FakeService;
+import view.GreenTeaView;
+
+/**
+ * @author adelfiri,
+ *         <a href="mailto:Konstantin.Molodtsov@returnonintelligence.com">
+ *         Konstantin Molodtsov</a>
+ *         
+ *         One to several
+ *         
+ * @since 03 April 2016
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/application-context.xml" })
+public class GreenTeaTest {
+
+	/*@Autowired
+	@Qualifier("teaServiceMapStructImpl")
+	@InjectMocks
+
+	/*
+	 * @Autowired
+	 * 
+	 * @Qualifier("teaServiceJMapperImpl")
+	 * 
+	 * @InjectMocks private TeaService teaService;
+	 */
+	
+	/*
+	 * @Autowired
+	 * 
+	 * @Qualifier("teaServiceDozerImpl")
+	 * 
+	 * @InjectMocks private TeaService teaService;
+	 */
+
+      @Autowired
+      @Qualifier("teaServiceModelMapperImpl")
+      @InjectMocks 
+      private TeaService teaService;
+	
+	/*
+	 * @Autowired
+	 * 
+	 * @Qualifier("teaServiceOrikaImpl")
+	 * 
+	 * @InjectMocks private TeaService teaService;
+	 */
+
+	@Mock
+	private FakeService fakeService;
+
+	@Before
+	public void initMocks() {
+
+		MockitoAnnotations.initMocks(this);
+
+		UserSession userSession = new UserSession();
+		userSession.setEmployee(true);
+		when(fakeService.getUserSession()).thenReturn(userSession);
+	}
+
+	@Test
+	public void constantTest() {
+		
+		// http://stackoverflow.com/questions/34297791/with-dozer-is-it-possible-to-map-several-fields-to-one-field
+		
+		// http://www.talangsoft.org/2015/04/20/domain_mapping_with_modelmapper/
+		
+		// http://stackoverflow.com/questions/29070118/modelmapper-mapping-from-multi-flat-object-to-hierarchy-object
+
+		GreenTeaView greenTeaView = teaService.findGreenTea(25l);
+
+		Assert.assertEquals(greenTeaView.getDescription(), "descriptioncontent");
+
+	}
+
+}
---
+++ src/test/java/tea/service/WhiteTeaTest.java
@@ -0,0 +1,94 @@
+package tea.service;
+
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.testng.Assert;
+
+import tea.service.impl.FakeService;
+import view.WhiteTeaView;
+
+/**
+ * @author adelfiri,
+ *         <a href="mailto:Konstantin.Molodtsov@returnonintelligence.com">
+ *         Konstantin Molodtsov</a>
+ *         
+ *         Constant test
+ *         
+ * @since 03 April 2016
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/application-context.xml" })
+public class WhiteTeaTest {
+
+	/*@Autowired
+	@Qualifier("teaServiceMapStructImpl")
+	@InjectMocks
+
+	/*
+	 * @Autowired
+	 * 
+	 * @Qualifier("teaServiceJMapperImpl")
+	 * 
+	 * @InjectMocks private TeaService teaService;
+	 */
+	
+	/*
+	 * @Autowired
+	 * 
+	 * @Qualifier("teaServiceDozerImpl")
+	 * 
+	 * @InjectMocks private TeaService teaService;
+	 */
+
+      @Autowired
+      @Qualifier("teaServiceModelMapperImpl")
+      @InjectMocks 
+      private TeaService teaService;
+	
+	/*
+	 * @Autowired
+	 * 
+	 * @Qualifier("teaServiceOrikaImpl")
+	 * 
+	 * @InjectMocks private TeaService teaService;
+	 */
+
+	@Mock
+	private FakeService fakeService;
+
+	@Before
+	public void initMocks() {
+
+		MockitoAnnotations.initMocks(this);
+
+		UserSession userSession = new UserSession();
+		userSession.setEmployee(true);
+		when(fakeService.getUserSession()).thenReturn(userSession);
+	}
+
+	@Test
+	public void constantTest() {
+
+		// http://stackoverflow.com/questions/858519/constants-in-dozer-mappings
+
+		WhiteTeaView whiteTeaView = teaService.findWhiteTea(25l);
+		
+		String variety = whiteTeaView.getVariety();
+
+		Assert.assertEquals(variety, "White Peony");
+
+
+
+	}
+
+}
